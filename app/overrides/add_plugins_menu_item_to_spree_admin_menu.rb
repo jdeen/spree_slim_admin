40c015550f714607c187e3163e58a3b370e57e36
Ziyan Junaideen <jdeen-solutions@outlook.com>
2015-01-22 12:15:32 +0530

    Deface overide - commented - need bower rails

---
+++ app/overrides/add_plugins_menu_item_to_spree_admin_menu.rb
@@ -0,0 +1,6 @@
+# Deface::Overide.new(
+#   :virtual_path => "app/views/spree/admin/shared/_menu",
+#   :name => "add_plugins_menu_item_to_spree_admin_menu",
+#   :insert_bottom => "[data-hook='admin_tabs']",
+#   :partial 
+# )
